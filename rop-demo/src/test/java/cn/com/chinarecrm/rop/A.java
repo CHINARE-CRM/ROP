02cfab6464b4b22a17cba8f949d5c75bda362b36
王贵源 <kerbores@gmail.com>
2019-09-30 11:22:02 +0800

    add: 客户端支持代理

--- rop-demo/src/test/java/cn/com/chinarecrm/rop/A.java
+++ rop-demo/src/test/java/cn/com/chinarecrm/rop/A.java
@@ -1,4 +1,9 @@
 package cn.com.chinarecrm.rop;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+
 import org.nutz.http.Request.METHOD;
 import org.nutz.http.Response;
 import org.nutz.lang.Lang;
@@ -11,8 +16,7 @@ import cn.com.chinarecrm.rop.client.ROPRequest;
 public class A {
     public static void main(String[] args) {
         ROPClient client = ROPClient.create("test", Lang.md5("test"), "http://127.0.0.1:8080/rop.endpoint", "SHA1");
-        // client.setProxy(new Proxy(Type.HTTP, new
-        // InetSocketAddress("127.0.0.1", 8888)));
+        client.setProxy(new Proxy(Type.HTTP, new InetSocketAddress("127.0.0.1", 8888)));
         Response response = client.send(ROPRequest.create("/test", METHOD.GET, NutMap.NEW().addv("id", R.random(0, 100))));
         System.err.println(response.getContent());
     }
